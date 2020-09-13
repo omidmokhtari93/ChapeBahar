3af8fd0ec03a5b7651958bc47d7ccf970ad78717
Omid <Omid@DESKTOP-LJM88T4>
2020-09-13 19:55:55 +0430

    register and theme changes

--- ChapeBahar/ClientApp/src/Components/Register/Register.js
+++ ChapeBahar/ClientApp/src/Components/Register/Register.js
@@ -7,28 +7,65 @@ const Register = props => {
         <div className="container">
             <div className="row justify-content-center">
                 <div className="col-12 col-md-5 col-xl-4 my-5">
-                    <Alert message="با موفقیت انجام شد" type="success" show/>
+                    <Alert message="با موفقیت انجام شد" type="success" show />
                     <div className="text-center mb-5">
-                        <img src={logo} height="60" alt="logo" />
-                    </div>
-                    <h1 className="display-4 text-center mb-3">ثبت نام</h1>
+                        <img src={logo} height="50" alt="logo" /></div>
+                    <h1 className="display-4 text-center mb-3">ایجاد حساب کاربری</h1>
                     <p className="text-muted text-center mb-5">ثبت نام در سایت</p>
-                    <form>
-                        <div className="form-group">
-                            <label>نام و نام خانوادگی</label>
-                            <input type="name" className="form-control" />
+                    <div className="card">
+                        <div className="card-header">
+                            <h4 className="card-header-title text-center">ایجاد حساب کاربری</h4>
                         </div>
-                        <div className="form-group">
-                            <label>شماره موبایل</label>
-                            <input type="mobile" className="form-control" />
+                        <div className="card-body">
+                            <form className="needs-validation" novalidate>
+
+                                <div className="form-group">
+                                    <label>نام و نام خانوادگی</label>
+                                    <input type="name" className="form-control" placeholder="محسن نصیری" required />
+                                </div>
+                                <div className="form-group">
+                                    <label>شماره موبایل</label>
+                                    <input type="username" className="form-control" placeholder="09357892060" required />
+                                </div>
+                                <div className="form-group">
+                                    <label>کد معرف</label>
+                                    <input type="username" className="form-control" placeholder="در صورت موجود وارد کنید" />
+                                </div>
+                                <div className="form-group">
+                                    <div className="custom-control custom-switch">
+                                        <input type="checkbox" className="custom-control-input" id="cardToggle"
+                                            data-toggle="chart" data-target="#conversionsChart" data-trigger="change"
+                                            data-action="add" data-dataset="1" />
+                                        <label className="custom-control-label" for="cardToggle"></label>
+                                        <a className="text-muted" href="#" data-toggle="modal" data-target="#terms"
+                                            style={{ lineHeight: '1.8' }}>قوانین را خوانده ام و آن را قبول دارم</a>
+                                    </div>
+                                </div>
+                                <div className="modal fade" id="terms" tabindex="-1" role="dialog" aria-labelledby="terms"
+                                    aria-hidden="true">
+                                    <div className="modal-dialog" role="document">
+                                        <div className="modal-content">
+                                            <div className="modal-header">
+                                                <h2 className="modal-title">قوانین سایت</h2>
+                                            </div>
+                                            <div className="modal-body">تست</div>
+                                            <div className="modal-footer">
+                                                <button type="button" className="btn btn-secondary"
+                                                    data-dismiss="modal">ادامه</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button className="btn btn-success btn-lg btn-block mb-3">ثبت نام</button>
+                            </form>
                         </div>
-                        <button className="btn btn-lg btn-block btn-primary mb-3">ثبت نام</button>
-                        <div className="text-center">
-                            <small className="text-muted text-center">
-                                حساب کاربری دارید؟ <a href="#">ورود</a>
-                            </small>
+                        <div className="card-footer">
+                            <div className="text-center">
+                                <div className="text-muted text-center mb-3">حساب کاربری دارید؟ </div>
+                            </div>
+                            <a className="btn btn-primary btn-lg btn-block" href="#">ورود</a>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
