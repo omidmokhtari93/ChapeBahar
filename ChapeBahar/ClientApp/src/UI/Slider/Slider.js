cdcf705d598e49638d392d64721c305d267ac061
IT2 <IT2@IT2>
2020-09-21 13:00:44 +0330

    products animation fixed

--- ChapeBahar/ClientApp/src/UI/Slider/Slider.js
+++ ChapeBahar/ClientApp/src/UI/Slider/Slider.js
@@ -7,18 +7,45 @@ import { isMobile } from 'react-device-detect';
 
 const ProductsSlider = prpos => {
     let sliderWrapper = useRef(null);
+    let slider = useRef(null);
     var documentWidth = document.body.clientWidth
+    let interaval = null, timeout = null;
+    const scrollLeft = e => {
+        interaval = setInterval(() => {
+            slider.current.scrollLeft -= 0.1
+            const { scrollWidth, scrollLeft, offsetWidth } = slider.current
+            if ((offsetWidth - scrollWidth) == (scrollLeft)) {
+                clearInterval(interaval)
+            }
+        }, 25);
+    }
+
+    const startScroll = e => {
+        timeout = setTimeout(() => {
+            scrollLeft()
+        }, 6000);
+    }
+
+    const stopScrolling = e => {
+        clearInterval(interaval)
+        clearTimeout(timeout)
+        timeout = null
+        interaval = null
+    }
 
     useEffect(() => {
         if (sliderWrapper.current) {
             if (isMobile) {
                 sliderWrapper.current.style.width = documentWidth - 30 + 'px';
+                startScroll()
             }
         }
     })
     return (
-        <div className="slide-wrap" ref={sliderWrapper}>
-            <div className="slider">
+        <div className="slide-wrap" ref={sliderWrapper}
+            onTouchStart={() => stopScrolling()}
+            onTouchEnd={() => startScroll()}>
+            <div className="slider" ref={slider}>
                 <div className="slide" id="slide-1">
                     <Product />
                 </div>
